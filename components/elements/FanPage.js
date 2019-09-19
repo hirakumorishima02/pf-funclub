fce7c3d899a574201b44953aad59a5815b2c70a1
hirakumorishima02 <business.ujinchu@gmail.com>
2019-09-19 14:22:59 +0900

    Create Serch Result

---
+++ components/elements/FanPage.js
@@ -0,0 +1,14 @@
+const FanPage = () => (
+        <div className="fan-page">
+            <h4>ファンクラブ名</h4>
+            <h5>アーティスト名</h5>
+            <style jsx>{`
+                .fan-page {
+                    border: 1px solid black;
+                    margin-bottom: 10px;
+                }
+            `}</style>
+        </div>
+)
+
+export default FanPage;
