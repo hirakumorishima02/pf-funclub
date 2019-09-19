35a855800f92afad80338d0d161d71ccc2e75592
hirakumorishima02 <business.ujinchu@gmail.com>
2019-09-19 17:39:21 +0900

    Create detail.js

---
+++ pages/detail.js
@@ -0,0 +1,22 @@
+const Detail = () => (
+    <div>
+        <h3>〇〇を応援する会</h3>
+        <p>
+            ファンクラブの説明です。　ファンクラブの説明です。　ファンクラブの説明です。　
+            ファンクラブの説明です。　ファンクラブの説明です。　ファンクラブの説明です。　
+            ファンクラブの説明です。　ファンクラブの説明です。　ファンクラブの説明です。　
+            ファンクラブの説明です。　ファンクラブの説明です。　ファンクラブの説明です。　
+        </p>
+        <img src="../static/fan-img.png" className="fan-img" /><br />
+        <button>入会</button>
+        <style jsx>{`
+            div {
+                margin: 0 auto;
+                width: 80%;
+                height: 80%;
+            }
+        `}</style>
+    </div>
+);
+
+export default Detail;
