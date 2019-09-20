a39da2ab579e282d58f31165cf117daa497c1a97
hirakumorishima02 <business.ujinchu@gmail.com>
2019-09-20 10:47:05 +0900

    Processing to connect MySQL

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import fetch from 'isomorphic-unfetch'
 import Link from 'next/link'
 
+// プロファイルとページネーションデータを受け取り、propsとして利用できるようにする
 HomePage.getInitialProps = async ({ req, query }) => {
+  // 要求がサーバー側から行われているかクライアント側から行われているかを確認
   const protocol = req
     ? `${req.headers['x-forwarded-proto']}:`
     : location.protocol
   const host = req ? req.headers['x-forwarded-host'] : location.host
+  // クエリパラメータを使用して、プロファイルのAPIにリクエストを行う
   const pageRequest = `${protocol}//${host}/api/posts?page=${query.page ||
     1}&limit=${query.limit || 9}`
   const res = await fetch(pageRequest)
