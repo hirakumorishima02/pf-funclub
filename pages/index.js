6fcdeb5ba67fe7340db20ae6604c5fbd18814f0c
hirakumorishima02 <business.ujinchu@gmail.com>
2019-09-21 17:46:23 +0900

    Success to connect firebase

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,43 @@
-import firebase from '../lib/db'
+import { loadFirebase } from '../lib/db'
 import React from 'react'
 
-// export default class Index extends React.Component {
-//   static async getInitialProps() {
-//     let db = firebase
-//   }
-// }
+ export default class Index extends React.Component {
+  static async getInitialProps() {
+    let firebase = await loadFirebase()
+    let result = await new Promise((resolve, reject) => {
+      firebase.firestore().collection('posts')
+      .get()
+      .then(snapshot => {
+        let data = []
+        snapshot.forEach((doc) => {
+          data.push(
+            Object.assign({
+              id: doc.id
+            }, doc.data())
+          )
+        })
+        // console.log(data)
+        resolve(data)
+      }).catch(error => {
+        reject([])
+      })
+    })
+    // console.log(result)
+    return {posts: result}
+  }
+  render() {
+    const posts = this.props.posts
+    return (
+      <ul>
+        {posts.map(post => <li key="{post.id}">{post.title}</li>)}
+      </ul>
+    );
+  }
+}
 
-console.log(firebase)
-const Index = () => (
-  <div>test</div>
-);
+// console.log(firebase)
+// const Index = () => (
+//   <div>test</div>
+// );
 
-export default Index;
+// export default Index;
