7611d15f84f12ffc5e0041e21a1d8638855f577d
hirakumorishima02 <business.ujinchu@gmail.com>
2019-09-27 15:14:37 +0900

    Stylying Header.js

--- components/shared/Header.js
+++ components/shared/Header.js
@@ -1,66 +1,225 @@
+import { auth, firebase } from "../../lib/db";
 import React from 'react';
 import Link from 'next/link';
-import Button from '@material-ui/core/Button';
-import { auth, firebase } from "../../lib/db";
 
-class Header extends React.Component {
-    handleSignIn = () => {
-        var provider = new firebase.auth.GoogleAuthProvider();
-        provider.addScope("https://www.googleapis.com/auth/contacts.readonly");
-        auth
-          .signInWithPopup(provider)
-          .then(() => {
-            alert("You are signed In");
-          })
-          .catch(err => {
-            alert("OOps something went wrong check your console");
-            console.log(err);
-          });
-      };
-      handleSignout = () => {
-        auth
-          .signOut()
-          .then(function() {
-            alert("Logout successful");
-          })
-          .catch(function(error) {
-            alert("OOps something went wrong check your console");
-            console.log(err);
-          });
-      };
+import { fade, makeStyles } from '@material-ui/core/styles';
 
-    render() {
-        return (
-            <div className="header">
-                <Link href='/'>
-                    <a>Fan Club</a>
-                </Link>
-                    <Button variant="contained" color="primary" onClick={this.handleSignIn}>
-                        SIGN IN
-                    </Button>
-                    <Button variant="contained" color="secondary" onClick={this.handleSignout}>
-                        SIGN OUT
-                    </Button>
-                    <Button href="/my-page" variant="contained">
-                        My Page
-                    </Button>
-                <form className="serch-form">
-                    <input type="text" placeholder="Serch Artists" />
-                    <Link href="/SearchResult">
-                        <button type="submit">Search</button>
-                    </Link>
-                </form>
-                <style jsx>{`
-                    .header {
-                        height: 80px;
-                    }
-                    .serch-form {
-                        display: inline-block;
-                    }
-                `}</style>
-            </div>
-        )
-    }
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MeetingRoom from '@material-ui/icons/MeetingRoom';
+import ExitToApp from '@material-ui/icons/ExitToApp';
+import MoreIcon from '@material-ui/icons/MoreVert';
+
+
+const useStyles = makeStyles(theme => ({
+    grow: {
+      flexGrow: 1,
+    },
+    title: {
+      display: 'none',
+      [theme.breakpoints.up('sm')]: {
+        display: 'block',
+      },
+    },
+    search: {
+      position: 'relative',
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: fade(theme.palette.common.white, 0.15),
+      '&:hover': {
+        backgroundColor: fade(theme.palette.common.white, 0.25),
+      },
+      marginRight: theme.spacing(2),
+      marginLeft: 0,
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(3),
+        width: 'auto',
+      },
+    },
+    searchIcon: {
+      width: theme.spacing(7),
+      height: '100%',
+      position: 'absolute',
+      pointerEvents: 'none',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    inputRoot: {
+      color: 'inherit',
+    },
+    inputInput: {
+      padding: theme.spacing(1, 1, 1, 7),
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('md')]: {
+        width: 200,
+      },
+    },
+    sectionDesktop: {
+      display: 'none',
+      [theme.breakpoints.up('md')]: {
+        display: 'flex',
+      },
+    },
+    sectionMobile: {
+      display: 'flex',
+      [theme.breakpoints.up('md')]: {
+        display: 'none',
+      },
+    },
+}));
+
+const handleSignIn = () => {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope("https://www.googleapis.com/auth/contacts.readonly");
+    auth
+        .signInWithPopup(provider)
+        .then(() => {
+        alert("You are signed In");
+        })
+        .catch(err => {
+        alert("OOps something went wrong check your console");
+        console.log(err);
+        });
+};
+
+const handleSignout = () => {
+    auth
+        .signOut()
+        .then(function() {
+        alert("Logout successful");
+        })
+        .catch(function(error) {
+        alert("OOps something went wrong check your console");
+        console.log(err);
+        });
+};
+
+const handleMyPage = () => {
+    document.location.href = "/my-page";
+};
+
+
+export default function Header() {
+    const classes = useStyles();
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+    const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+    const handleMobileMenuClose = () => {
+      setMobileMoreAnchorEl(null);
+    };
+
+    const handleMenuClose = () => {
+      setAnchorEl(null);
+      handleMobileMenuClose();
+    };
+
+    const handleMobileMenuOpen = event => {
+      setMobileMoreAnchorEl(event.currentTarget);
+    };
+
+    const menuId = 'primary-search-account-menu';
+
+    const mobileMenuId = 'primary-search-account-menu-mobile';
+
+    const renderMobileMenu = (
+      <Menu
+        anchorEl={mobileMoreAnchorEl}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+        id={mobileMenuId}
+        keepMounted
+        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+        open={isMobileMenuOpen}
+        onClose={handleMobileMenuClose}
+      >
+        <MenuItem>
+          <IconButton color="inherit">
+              <MeetingRoom onClick={handleSignIn}/>
+          </IconButton>
+          <p>Sign in</p>
+        </MenuItem>
+        <MenuItem>
+          <IconButton color="inherit">
+              <ExitToApp onClick={handleSignout}/>
+          </IconButton>
+          <p>Sign out</p>
+        </MenuItem>
+        <MenuItem>
+          <IconButton
+            aria-label="account of current user"
+            aria-controls="primary-search-account-menu"
+            aria-haspopup="true"
+            color="inherit"
+          >
+            <AccountCircle onClick={handleMyPage}/>
+          </IconButton>
+          <p>My Page</p>
+        </MenuItem>
+      </Menu>
+    );
+
+    return (
+        <div className={classes.grow}>
+          <AppBar position="static">
+            <Toolbar>
+              <Typography className={classes.title} variant="h6" noWrap>
+                Fan Club
+              </Typography>
+
+
+              <div className={classes.search}>
+                <div className={classes.searchIcon}>
+                  <SearchIcon />
+                </div>
+                <InputBase
+                  placeholder="Search…"
+                  classes={{
+                    root: classes.inputRoot,
+                    input: classes.inputInput,
+                  }}
+                  inputProps={{ 'aria-label': 'search' }}
+                />
+              </div>
+
+
+              <div className={classes.grow} />
+              <div className={classes.sectionDesktop}>
+                <IconButton color="inherit">
+                    <MeetingRoom onClick={handleSignIn}/>
+                </IconButton>
+                <IconButton color="inherit">
+                    <ExitToApp onClick={handleSignout}/>
+                </IconButton>
+                <IconButton color="inherit">
+                  <AccountCircle onClick={handleMyPage}/>
+                </IconButton>
+              </div>
+
+
+              <div className={classes.sectionMobile}>
+                <IconButton
+                  aria-haspopup="true"
+                  onClick={handleMobileMenuOpen}
+                  color="inherit"
+                >
+                  <MoreIcon />
+                </IconButton>
+              </div>
+
+            </Toolbar>
+          </AppBar>
+          {renderMobileMenu}
+        </div>
+      );
 }
-
-export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { db } from '../lib/db';
 import React from 'react';
 import Link from 'next/link';
+
 import Header from '../components/shared/Header';
 import Footer from '../components/shared/Footer';
 import SiteDesctiption from '../components/index/Site-description';
@@ -10,79 +11,73 @@ import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
 
-// import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 
-// const useStyles = makeStyles(theme => ({
-//   popularArtists: {
-//     display: 'flex',
-//     flexWrap: 'wrap',
-//     justifyContent: 'space-around',
-//     overflow: 'hidden',
-//     backgroundColor: theme.palette.background.paper,
-//   },
-//   gridList: {
-//     flexWrap: 'nowrap',
-//     transform: 'translateZ(0)',
-//   },
-// }));
+const useStyles = makeStyles(theme => ({
 
-export default class Index extends React.Component {
-  static async getInitialProps() {
-    let result = await new Promise((resolve, reject) => {
-      db.collection('fanPages')
-      .get()
-      .then(snapshot => {
-        let data = []
-        snapshot.forEach((doc) => {
-          data.push(
-            Object.assign({
-              id: doc.id
-            }, doc.data())
-          )
-        })
-        resolve(data)
-      }).catch(error => {
-        reject([])
-      })
-    })
-    return {fanPages: result}
-  }
+  popularArtists: {
+    display:         'flex',
+    flexWrap:        'wrap',
+    justifyContent:  'space-around',
+    overflow:        'hidden',
+    backgroundColor: theme.palette.background.paper,
+  },
 
-  render() {
-    // const classes = useStyles();
-    const fanPages = this.props.fanPages
-    console.log(fanPages)
+  gridList: {
+    flexWrap:  'nowrap',
+    transform: 'translateZ(0)',
+  },
+
+}));
+
+const Index = ({fanPages}) => {
+    const classes = useStyles();
     return (
-    <div>
-        <Header />
+        <>
+            <Header />
+            <Wallpaper />
+            <SiteDesctiption />
 
-        {/* トップ画像 */}
-        <Wallpaper />
-        {/* サイトの説明 */}
-        <SiteDesctiption />
-        {/* 人気なアーティストの一覧 */}
-        <div>
-            <h3>人気なアーティスト一覧</h3>
-            {/* アーティスト画像の表示 */}
             <div>
-                {fanPages.map(fanPage =>
-                <GridList>
-                    <GridListTile>
-                        <Link href="/p/[detailid]" as={`/p/${fanPage.id}`}>
-                        <img src="../static/popular-artist-img.jpg" />
-                        </Link>
-                        <GridListTileBar title={fanPage.artistName} />
-                    </GridListTile>
-                </GridList>
+                <h3>人気なアーティスト一覧</h3>
+                <div className={classes.popularArtists}>
+                    {fanPages.map(fanPage =>
+                        <GridList className={classes.gridList} cols={2.5} key={fanPage.id}>
+                            <GridListTile>
+                                <Link href="/p/[detailid]" as={`/p/${fanPage.id}`}>
+                                    <img src="../static/popular-artist-img.jpg" />
+                                </Link>
+                                <GridListTileBar title={fanPage.artistName} />
+                            </GridListTile>
+                        </GridList>
                     )}
+                </div>
             </div>
-        </div>
 
-        <Footer />
-        </div>
-        );
-        }
-    }
+            <Footer />
+        </>
+    );
+}
 
-{/* <div className={classes.popularArtists}>
-<GridList className={classes.gridList} cols={2.5}> */}
+Index.getInitialProps =
+
+    async() => {
+        let result =
+                await
+                    db.collection('fanPages')
+                    .get()
+                    .then(snapshot => {
+                        let data = []
+                        snapshot.forEach((doc) => {
+                            data.push(
+                            Object.assign({id: doc.id}, doc.data()))})
+                        return data
+                    }).catch(error => {
+                        return []
+    })
+
+    return {fanPages: result}
+
+};
+
+export default Index;
