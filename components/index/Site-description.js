4d0200e11d59c4b597c3ab687c323eba312ba077
hirakumorishima02 <business.ujinchu@gmail.com>
2019-09-27 15:49:38 +0900

    Styling Site-description.js

--- components/index/Site-description.js
+++ components/index/Site-description.js
@@ -1,37 +1,60 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+        backgroundColor: '#3f51b5',
+    },
+    control: {
+      padding: theme.spacing(2),
+    },
+  }));
+
 
 export default function SiteDesctiption() {
+    const [spacing, setSpacing] = React.useState(2);
+    const classes = useStyles();
+
     return (
-        <div className="site-features">
-        <h3>サイトの特徴</h3>
-        <Grid container>
-            {/* 特徴1 */}
-            <Grid item sm={4}>
-                <h4>特徴１</h4>
-                <p>
-                    サイトの特徴です。　サイトの特徴です。　サイトの特徴です。　サイトの特徴です。　
-                    サイトの特徴です。　サイトの特徴です。　サイトの特徴です。　サイトの特徴です。　
-                    サイトの特徴です。　サイトの特徴です。　サイトの特徴です。　サイトの特徴です。　
-                </p>
+    <div className="site-features">
+        <Grid container className={classes.root} spacing={2} justify="center">
+            <Grid item xs={4} md={3}>
+                <Paper className={classes.paper}>
+                    <h4>特徴</h4>
+                    <p>
+                        特徴の説明　特徴の説明　特徴の説明　特徴の説明　
+                        特徴の説明　特徴の説明　特徴の説明　特徴の説明　
+                        特徴の説明　特徴の説明　特徴の説明　特徴の説明　
+                        特徴の説明　特徴の説明　特徴の説明　特徴の説明　
+                    </p>
+                </Paper>
             </Grid>
-            {/* 特徴2 */}
-            <Grid item sm={4}>
-            <h4>特徴2</h4>
-                <p>
-                    サイトの特徴です。　サイトの特徴です。　サイトの特徴です。　サイトの特徴です。　
-                    サイトの特徴です。　サイトの特徴です。　サイトの特徴です。　サイトの特徴です。　
-                    サイトの特徴です。　サイトの特徴です。　サイトの特徴です。　サイトの特徴です。　
-                </p>
+            <Grid item xs={4} md={3}>
+                <Paper className={classes.paper}>
+                    <h4>特徴</h4>
+                    <p>
+                        特徴の説明　特徴の説明　特徴の説明　特徴の説明　
+                        特徴の説明　特徴の説明　特徴の説明　特徴の説明　
+                        特徴の説明　特徴の説明　特徴の説明　特徴の説明　
+                        特徴の説明　特徴の説明　特徴の説明　特徴の説明　
+                    </p>
+                </Paper>
             </Grid>
-            {/* 特徴3 */}
-            <Grid item sm={4}>
-            <h4>特徴3</h4>
-                <p>
-                    サイトの特徴です。　サイトの特徴です。　サイトの特徴です。　サイトの特徴です。　
-                    サイトの特徴です。　サイトの特徴です。　サイトの特徴です。　サイトの特徴です。　
-                    サイトの特徴です。　サイトの特徴です。　サイトの特徴です。　サイトの特徴です。　
-                </p>
+            <Grid item xs={4} md={3}>
+                <Paper className={classes.paper}>
+                    <h4>特徴</h4>
+                    <p>
+                        特徴の説明　特徴の説明　特徴の説明　特徴の説明　
+                        特徴の説明　特徴の説明　特徴の説明　特徴の説明　
+                        特徴の説明　特徴の説明　特徴の説明　特徴の説明　
+                        特徴の説明　特徴の説明　特徴の説明　特徴の説明　
+                    </p>
+                </Paper>
             </Grid>
         </Grid>
     </div>
