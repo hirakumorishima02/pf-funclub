726877192486a2d3fc4b01f44c751a87afa19f88
hirakumorishima02 <business.ujinchu@gmail.com>
2019-10-26 11:07:16 +0900

    Removed Seach Form from Header.js

--- components/shared/Header.js
+++ components/shared/Header.js
@@ -8,10 +8,8 @@ import AppBar     from '@material-ui/core/AppBar';
 import Toolbar    from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import InputBase  from '@material-ui/core/InputBase';
 import MenuItem   from '@material-ui/core/MenuItem';
 import Menu       from '@material-ui/core/Menu';
-import SearchIcon from '@material-ui/icons/Search';
 
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import MeetingRoom   from '@material-ui/icons/MeetingRoom';
@@ -29,30 +27,6 @@ const useStyles = makeStyles(theme => ({
       [theme.breakpoints.up('sm')]: {
         display: 'block',
       },
-    },
-    search: {
-      position: 'relative',
-      borderRadius: theme.shape.borderRadius,
-      backgroundColor: fade(theme.palette.common.white, 0.15),
-      '&:hover': {
-        backgroundColor: fade(theme.palette.common.white, 0.25),
-      },
-      marginRight: theme.spacing(2),
-      marginLeft: 0,
-      width: '100%',
-      [theme.breakpoints.up('sm')]: {
-        marginLeft: theme.spacing(3),
-        width: 'auto',
-      },
-    },
-    searchIcon: {
-      width: theme.spacing(7),
-      height: '100%',
-      position: 'absolute',
-      pointerEvents: 'none',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
     },
     inputRoot: {
       color: 'inherit',
@@ -186,21 +160,6 @@ export default function Header() {
                   <a>Fan Club</a>
                 </Link>
               </Typography>
-
-              <div className={classes.search}>
-                <div className={classes.searchIcon}>
-                  <SearchIcon />
-                </div>
-                <InputBase
-                  placeholder="Search…"
-                  classes={{
-                    root: classes.inputRoot,
-                    input: classes.inputInput,
-                  }}
-                  inputProps={{ 'aria-label': 'search' }}
-                />
-              </div>
-
 
               <div className={classes.grow} />
               <div className={classes.sectionDesktop}>
